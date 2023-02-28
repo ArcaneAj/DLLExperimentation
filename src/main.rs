34961894fd419cbf4fa8af5cef01059316d091a5
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2023-02-28 21:02:11 +1300

    Added call to "multiply" function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    match extern_multiply(2, 3) {
+        Ok(result) => println!("{}", result),
+        Err(_) => println!("Uh oh!")
+    }
+}
+
+fn extern_multiply(x: i64, y: i64) -> Result<i64, Box<dyn std::error::Error>> {
+    unsafe {
+        let lib = libloading::Library::new("CppArithmetic/x64/Release/CppArithmetic.dll")?;
+        let func: libloading::Symbol<unsafe extern fn(x: i64, y: i64) -> i64> = lib.get(b"multiply")?;
+        Ok(func(x, y))
+    }
 }
